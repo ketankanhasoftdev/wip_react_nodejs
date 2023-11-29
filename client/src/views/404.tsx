d8bb974bb223c9d7960e2b99cf571e87a631750c
Ketan Modi <ketanmodi@kanhasofttech.com>
2023-11-29 11:23:47 +0530

    page not found changes

--- client/src/views/404.tsx
+++ client/src/views/404.tsx
@@ -1,7 +1,12 @@
+import { Box } from "@mui/joy";
 import React from "react";
 
 const PageNotFound = () => {
-  return <div>PageNotFound</div>;
+  return (
+    <Box sx={{ textAlign: "center" }}>
+      <img src="/assets/images/404.png" alt="" />
+    </Box>
+  );
 };
 
 export default PageNotFound;
